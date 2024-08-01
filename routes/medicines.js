cb3d5c825c48eb4b38a2c5386115afa3599377a5
Prakriti Garg <157406959+praxcodes@users.noreply.github.com>
2024-08-01 13:45:11 +0530

    Update medicines.js

--- routes/medicines.js
+++ routes/medicines.js
@@ -1,40 +1,26 @@
-const express = require('express'); // Import the Express library
+const express = require('express');
+const router = express.Router();
+const { redisCacheMiddleware } = require('../middleware/redis'); // Importing Redis caching middleware
 
-const medicines = require('./routes/medicines'); // Import the medicines routes
-const connectDB = require('./db/connect'); // Import the database connection function
-require('dotenv').config(); // Load environment variables from a .env file
-const notFound = require('./middleware/not-found'); // Import the 404 middleware
-const redis = require('redis'); // Import the Redis library
-const { initialiseRedisClient } = require('./middleware/redis'); // Import the Redis initialization function
+// Importing controller functions for handling requests
+const {
+    getAllMedicines,
+    createMedicine,
+    getMedicine,
+    updateMedicine,
+    deleteMedicine,
+} = require('../controllers/medicines');
 
-// Middleware
-async function initialiseExpress() {
-    const app = express(); // Create an Express application
-    app.use(express.json()); // Middleware to parse JSON request bodies
+// Route for getting all medicines and creating a new medicine
+// Applies Redis caching middleware to the GET request
+router.route('/')
+    .get(redisCacheMiddleware(), getAllMedicines) // Cache the response for getting all medicines
+    .post(createMedicine); // Create a new medicine
 
-    await initialiseRedisClient(); // Initialize the Redis client
-    app.use('/api/v1/medicines', medicines); // Use the medicines routes
-    app.use(notFound); // Middleware to handle 404 errors
+// Route for getting, updating, and deleting a specific medicine by ID
+router.route('/:id')
+    .get(getMedicine) // Get a single medicine by ID
+    .patch(updateMedicine) // Update a single medicine by ID
+    .delete(deleteMedicine); // Delete a single medicine by ID
 
-    const port = process.env.port || 3000; // Define the port from environment variables or default to 3000
-
-    // Function to start the server and connect to the database
-    const start = async () => {
-        try {
-            await connectDB(process.env.MONGO_URI); // Connect to the MongoDB database
-            app.listen(port, () =>
-                console.log(`Server is listening on port ${port}...`)
-            );
-        } catch (error) {
-            console.log(error); // Log any errors
-        }
-    };
-
-    start(); // Start the server
-}
-
-// Initialize the Express application
-initialiseExpress()
-    .then()
-    .catch((e) => console.log(e)); // Log any errors during initialization
-
+module.exports = router; // Exporting the router
