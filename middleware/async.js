34146ad83e55a4651d13ad2c843c3c177a4d3621
Prakriti Garg <157406959+praxcodes@users.noreply.github.com>
2024-08-01 13:41:42 +0530

    Update async.js

--- middleware/async.js
+++ middleware/async.js
@@ -1,10 +1,15 @@
-const asyncWrapper=(fn)=>{
-return async(req,res,next)=>{
-    try {
-        await fn(req,res,next)
-    } catch (error) {
-        next(error)
+//not using asyncWrapper in the current code, can be updated as an alternative
+// Wrapper function to handle asynchronous route handlers
+const asyncWrapper = (fn) => {
+    return async (req, res, next) => {
+        try {
+            // Execute the passed-in asynchronous function
+            await fn(req, res, next);
+        } catch (error) {
+            // Pass any errors to the next middleware/error handler
+            next(error);
+        }
     }
 }
-}
-module.exports=asyncWrapper
+
+module.exports = asyncWrapper;
